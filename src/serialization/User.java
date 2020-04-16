f39d718e149b00ee697cb86b2c5022fba3000858
dahun428 <rubcustomer@gmail.com>
2020-04-16 20:44:08 +0900

    '20200416'

--- src/serialization/User.java
+++ src/serialization/User.java
@@ -4,6 +4,13 @@ import java.io.Serializable;
 
 public class User implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
 	private int no;
 	private String userId;
 	private String password;
